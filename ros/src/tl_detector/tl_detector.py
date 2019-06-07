cd090bae6fb2229eef4d8b0fce572b4271f8fc71
gaurav2205 <gaurav.garg@gmx.de>
2019-06-07 18:15:40 +0200

    Update tl_detector.py

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -94,18 +94,20 @@ class TLDetector(object):
         '''
 		
 		#### Error here::: the waypoint is not getting called 
+        # Ensure that the light state hasn't changed before taking any option
         if self.state != state:
             self.state_count = 0
             self.state = state
+        elif self.state_count >= STATE_COUNT_THRESHOLD:
+            self.last_state = self.state
+            light_wp = light_wp if state == TrafficLight.RED or state == TrafficLight.YELLOW else -1
+            self.last_wp = light_wp
+			print("The waypoint published is ",light_wp)
+            self.upcoming_red_light_pub.publish(Int32(light_wp))
         else:
-			if self.state_count >= STATE_COUNT_THRESHOLD:
-				self.last_state = self.state
-				if state != TrafficLight.RED or state != TrafficLight.YELLOW:
-					light_wp = -1
-				self.last_wp = light_wp
-				print("waypoint published ", self.last_wp)
-				self.upcoming_red_light_pub.publish(self.last_wp)
-			self.state_count += 1
+			print("The waypoint published is ",self.last_wp)
+            self.upcoming_red_light_pub.publish(Int32(self.last_wp))
+        self.state_count += 1
 
     def get_closest_waypoint(self, x,y):
         """Identifies the closest path waypoint to the given position
