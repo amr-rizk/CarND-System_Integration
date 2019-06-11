d42814414d917c6685748c7e096025bc9dd90fc2
tomatsu999 <tomatsu999@gmail.com>
2019-06-11 22:09:04 +0900

    feat: Update classifier model and added training dataset
    - Update classifier model.
    - Added dataset.(Alex Lechner's dataset)

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -9,14 +9,14 @@ import cv2
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        self.resized_width=32
-        self.resized_height=32
+        self.resized_width=256
+        self.resized_height=192
         self.sess=tf.Session()
         self.saver=tf.train.import_meta_graph("./classifier_model/tl_classifier.ckpt.meta")
         self.saver.restore(self.sess, "./classifier_model/tl_classifier.ckpt")
         self.x_input=self.sess.graph.get_tensor_by_name("x_input:0")
         self.prediction=self.sess.graph.get_tensor_by_name("prediction:0")
-        self.data=np.zeros([1,self.resized_height,self.resized_width,3])
+        self.data=np.zeros([1,self.resized_width,self.resized_height,3])
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -32,6 +32,6 @@ class TLClassifier(object):
         image_hsv=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         self.data[0,:,:,:]=cv2.resize(image_hsv,(self.resized_height,self.resized_width))
         #tl_state_pred=self.prediction.eval(feed_dict={self.x_input: self.data})
-        tl_state_pred=self.sess.run("prediction:0",feed_dict={"x_input:0":self.data})[0]
+        tl_state_pred=self.sess.run("prediction:0",feed_dict={"x_input:0":self.data,"keep_prob:0":1.0})[0]
         #RED=0 YELLOW=1 GREEN=2
         return tl_state_pred
